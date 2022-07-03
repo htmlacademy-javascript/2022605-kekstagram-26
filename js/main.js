92de9a1bfaecefa1509411f77406ed4713bdcea5
Artem-Olkhovoi <likeajakob@mail.ru>
2022-07-04 02:44:56 +0300

    добавляет фотографии

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-function getRandomIntInclusive(min, max) {
-  if (min >= max) {
-    console.log('Задано некорректное максимальное значение. Оно должно быть больше минимального');
-  }
-
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-getRandomIntInclusive(0, 15);
-
 function checkCommentLength(line, maxLenght) {
   if (line.length <= maxLenght) {
     return true;
@@ -16,4 +5,66 @@ function checkCommentLength(line, maxLenght) {
     return false;
   }
 }
-checkCommentLength('12', 140);
+
+const NAMES = ['Иван', 'Сергей', 'Генадий', 'Сидоджи', 'Рик', 'Морти', 'Зена', 'Небула', 'Моника', 'Хлоя', 'Вова Вист',];
+const DESCRIPTIONS = ['Синее море', 'Зеленый лес', 'Черная дыра', 'Белые горы', 'Желтое поле', 'Серые будни', 'Розовый закат', 'Оранжевый кирпич',];
+const MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const PHOTOS_AMOUNT = 25;
+
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomPositiveInteger(0, elements.length - 1)];
+};
+
+const makePhoto = (id, url,description, likes, comments) => ({
+  id,
+  url,
+  description,
+  likes,
+  comments: {
+    id,
+    avatar,
+    message,
+    name,
+  }
+});
+
+let commentIdCounter = 1;
+
+const createPhotoComments = () => ({
+  id: commentIdCounter++,
+  avatar: `img/avatar-${getRandomPositiveInteger(1,6)}.svg`,
+  message: getRandomArrayElement(MESSAGE),
+  name:  getRandomArrayElement(NAMES),
+})
+
+const makePhotos = (count) => {
+  const photos = [];
+  for (let i = 1; i <= count; i++) {
+    photos.push({
+      id: i,
+      url: `photos/${i}.jpg`,
+      description: getRandomArrayElement(DESCRIPTIONS),
+      likes: getRandomPositiveInteger(15, 200),
+      comments: Array.from({length: 2}, createPhotoComments)
+    });
+  }
+  return photos;
+}
+
+const photos = makePhotos(PHOTOS_AMOUNT);
+console.log(photos);
